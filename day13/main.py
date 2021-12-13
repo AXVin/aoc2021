1d2b3f724f19681ae6b9aa2ccef642640ea6a3df
AXVin <35896758+AXVin@users.noreply.github.com>
2021-12-13 11:05:44 +0100

    Working day 13

--- day13/main.py
+++ day13/main.py
@@ -18,9 +18,10 @@ def part1(data):
         max_x = max(points, key=lambda x: x[0])[0] + 1
         max_y = max(points, key=lambda x: x[1])[1] + 1
 
-        instruction = instruction.strip()
-        axis, i = instruction[-3], instruction[-1]
+        instruction = instruction.strip().split(' ')[-1]
+        axis, i = instruction.split('=')
         i = int(i)
+        # print(axis, i)
         if axis == "x":
             for a, b in zip(range(i, -1, -1), range(i, max_x)):
                 for y in range(max_y):
@@ -32,24 +33,48 @@ def part1(data):
                     points[(x, a)] += points[(x, b)]
                     del points[(x, b)]
         break
-
-    ret = ""
-    max_x = max(points, key=lambda x: x[0])[0] + 1
-    max_y = max(points, key=lambda x: x[1])[1] + 1
-    for y in range(max_y):
-        for x in range(max_x):
-            ret += '#' if points[(x, y)] else '.'
-        ret += "\n"
-    # if max_y < 20:
-    print(ret)
     return sum(1 for value in points.values() if value)
 
 
 
 @test()
 def part2(data):
-    data = data.strip().split("\n")
+    point_lines, instructions = data.strip().split("\n\n")
     # data = list(map(int, data))
+    points = defaultdict(int)
+    for line in point_lines.split("\n"):
+        x, y = line.strip().split(",")
+        points[(int(x), int(y))] = 1
+
+    for instruction in instructions.split("\n"):
+        max_x = max(points, key=lambda x: x[0])[0] + 1
+        max_y = max(points, key=lambda x: x[1])[1] + 1
+
+        instruction = instruction.strip().split(' ')[-1]
+        axis, i = instruction.split('=')
+        i = int(i)
+        # print(axis, i)
+        if axis == "x":
+            for a, b in zip(range(i, -1, -1), range(i, max_x)):
+                for y in range(max_y):
+                    points[(a, y)] += points[(b, y)]
+                    del points[(b, y)]
+        else:
+            for a, b in zip(range(i, -1, -1), range(i, max_y)):
+                for x in range(max_x):
+                    points[(x, a)] += points[(x, b)]
+                    del points[(x, b)]
+
+    ret = ""
+    max_x = max(points, key=lambda x: x[0])[0] + 1
+    max_y = max(points, key=lambda x: x[1])[1] + 1
+    for y in range(max_y):
+        for x in range(max_x):
+            ret += '#' if points[(x, y)] else ' '
+        ret += "\n"
+    print(ret)
+    return sum(1 for value in points.values() if value)
+
 
 
 
